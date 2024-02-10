890a114abe640ea11db82e2b082cf688719f1f83
Gaurav-714 <gauravnagose714@gmail.com>
2024-02-10 15:00:48 +0530

    cart

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -26,7 +26,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ecom_site',
-    'ckeditor',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +117,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
@@ -132,3 +132,10 @@ EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+
+
+###
+from django.conf import settings
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
+settings.configure()
